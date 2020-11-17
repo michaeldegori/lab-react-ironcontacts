938a0555070fc0e4cff62a43c576f51e7f981b3e
Michael DeGori <michael.degori@gmail.com>
2020-11-17 15:39:22 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,94 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import contacts from './contacts.json';
 
 function App() {
+  let [first5, setFirst5] = useState(contacts.splice(0, 5))
+  let [restOfContacts, setRestOfContacts] = useState(contacts)
+
+  const DisplayFirst5 = () => {
+    return first5.map(eachContact => {
+      return (
+        <tr key={eachContact.id}>
+          <td><img style={{ width: '50px' }} src={eachContact.pictureUrl} alt="" /></td>
+          <td>{eachContact.name}</td>
+          <td>{eachContact.popularity}</td>
+          <td><button onClick={deleteContact} className="delete">Delete</button></td>
+        </tr>
+      )
+    })
+  }
+
+  const getRandomContact = () => {
+    let copyFirst5 = [...first5]
+    let copyRestOfContacts = [...restOfContacts]
+
+    let randomNum = Math.floor(Math.random() * restOfContacts.length)
+    let randomContact = restOfContacts[randomNum]
+
+    copyFirst5.push(randomContact)
+    copyRestOfContacts.splice(randomNum, 1)
+    setFirst5(copyFirst5)
+    setRestOfContacts(copyRestOfContacts)
+  }
+
+  const sortByName = () => {
+    let copyFirst5 = [...first5]
+
+    copyFirst5.sort((a, b) => {
+      if (a.name < b.name) {
+        return -1
+      } else {
+        return 1
+      }
+    });
+
+    setFirst5(copyFirst5)
+  }
+
+  const sortByPopularity = () => {
+    let copyFirst5 = [...first5]
+
+    copyFirst5.sort((a, b) => {
+      if (a.popularity < b.popularity) {
+        return 1
+      } else {
+        return -1
+      }
+    });
+
+    setFirst5(copyFirst5)
+  }
+
+  const deleteContact = index => {
+    let copyFirst5 = [...first5]
+
+    copyFirst5.splice(index, 1)
+    setFirst5(copyFirst5)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>IronContacts</h1>
+      <div class="body">
+        <button onClick={getRandomContact}>Add Random Contact</button>
+        <button onClick={sortByName}>Sort By Name</button>
+        <button onClick={sortByPopularity}>Sort By Popularity</button>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Popularity</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <DisplayFirst5 />
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
